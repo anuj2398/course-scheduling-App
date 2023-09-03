86bf7838dac19583ddad070d51f9c3d653b10f0b
anuj2398 <anujdeka98@gmail.com>
2023-09-03 17:53:26 +0530

    removing main test

--- java-maven-starter-kit/src/test/java/com/example/geektrust/MainTest.java
+++ java-maven-starter-kit/src/test/java/com/example/geektrust/MainTest.java
@@ -1,5 +1,6 @@
 package com.example.geektrust;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
@@ -8,18 +9,39 @@ import java.io.PrintStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class MainTest {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+import com.example.geektrust.services.FileProcesserService;
 
-    @BeforeEach
-    public void setUp(){
-        System.setOut(new PrintStream(outContent));
-    }
-    @Test
-    public void testMain(){
-        Main.main(new String[]{
-            "sample_input\\input1.txt"
-        });
-        assertTrue(!outContent.toString().trim().isEmpty());
-    }
+public class MainTest {
+    // private Main app;
+    // private FileProcesserService fileProcessorService;
+
+    // @BeforeEach
+    // public void setUp() {
+    //     app = new Main();
+    //     fileProcessorService = mock(FileProcesserService.class);
+    // }
+
+    // @Test
+    // public void testPrintUsage() {
+    //     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    //     System.setErr(new PrintStream(outContent));
+
+    //     app.printUsage();
+
+    //     String expectedOutput = "Usage: java CourseSchedulingApp <input-file>\n";
+    //     assertEquals(expectedOutput, outContent.toString());
+    // }
+
+    // @Test
+    // public void testHandleException() {
+    //     ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    //     System.setErr(new PrintStream(errContent));
+
+    //     Exception testException = new Exception("Test exception");
+    //     app.handleException(testException);
+
+    //     assertTrue(errContent.toString().contains("Error: Test exception"));
+    // }
+
+    // You can add more tests for other methods if necessary
 }
